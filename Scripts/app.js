d3f81abdf16b5ebb1a4971963c8c274028bf6ac8
Devilfer <sukhpartap55@gmail.com>
2017-03-24 17:16:14 -0400

    to downloaad in small

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,11 +1,31 @@
 
 
-
+//IFFE
 (function(){
     
-$('.carousel').carousel({
-interval: false
-});
+    "use strict"
+    let topoffset = 50; // var for top height
+    // activate scroll spy
+    $('body').scrollspy({
+        target:'header .navbar',
+        offset:topoffset
+    });
+    
+     let hash = $(this).find('li.active a').attr('href');
+        if(hash !=='#featured'){
+            $('header nav').addClass('inbody');
+        } else {
+            $('header nav').removeClass('inbody');
+        }
+   
+
+   
+
+    /*First Sliders */
+    $('.carousel').carousel({
+    interval: false
+    });
+
 
 
 })();
